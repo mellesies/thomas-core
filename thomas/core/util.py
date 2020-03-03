d7e1be2a03381fd36b5aede9bbd4e601a2035aab
Melle Sieswerda <melle@sieswerda.net>
2020-03-03 16:26:52 +0100

    Added support for .net file format.

--- thomas/core/util.py
+++ thomas/core/util.py
@@ -3,3 +3,6 @@
 
 def sep(default='-', nchar=80):
     print(default * nchar)
+
+# https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-list-of-lists
+flatten = lambda l: [item for sublist in l for item in sublist]
